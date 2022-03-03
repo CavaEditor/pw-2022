a5d61b6ab3610393dd919cae61918a06f1ad8ead
CavaEditor <cabacv15@gmail.com>
2022-03-03 16:47:53 +0200

    exercitiul patru v.2 DOM. fon pentru pare si impare

--- homework2/exfour.js
+++ homework2/exfour.js
@@ -1,9 +1,18 @@
 //lista OL
 const patru = document.getElementById("patru");
+const lista = document.getElementsByTagName("li")
 
+for(let i = 2; i < 20; i++){
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(i));
+    patru.appendChild(li);
+}
 
-    for(let i = 2; i < 20; i++){
-        const li = document.createElement("li");
-        li.appendChild(document.createTextNode(i));
-        patru.appendChild(li);
+for(let i = 0; i < lista.length; i ++){
+    if(i % 2 !== 0){
+        lista[i].style.background = "violet"
+    } else {
+        lista[i].style.background = "green"
     }
+}
+
