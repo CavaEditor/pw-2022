c77b48dea1088cca9b7ccdf477498bb6c58c3b91
CavaEditor <cabacv15@gmail.com>
2022-03-03 19:27:40 +0200

    inceputul temei JSON

---
+++ homework3/exone.js
@@ -0,0 +1,38 @@
+//declararea json
+const worker = {
+    npp: {
+        sureName: "nume",
+        lastName: "familie",
+        patronymic: "patronimic"
+    } ,
+    birthday: {
+        bDate: 15,
+        bMonth: "iunie",
+        bYear: 2001,
+    },
+    mobile: "+37367502300",
+    adress: {
+        city: "Balti",
+        street: "Ostrovski",
+        number: 18,
+    },
+    email: "cabacv15@gmail.com",
+    gender: "f",
+    position: "inginere-programmer",
+    dateStartWork: {
+        sDate: 10,
+        sMonth: "noiembrie",
+        sYear: 2021,
+    },
+    IDNP: "023165468",
+    salary: 80000,
+    placeBirth: "Balti",
+    totalWorkExperience: 1,
+    departament: "ti",
+    homePhoneNumber: "022274161",
+    workSchedule : {
+        start: "15:00",
+        end: "19:00",
+    },
+}
+//const w = JSON.parse(worker);
