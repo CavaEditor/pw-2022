b5d74cb7bdef593b4eca6f5d0ddc1c9197069c4e
CavaEditor <cabacv15@gmail.com>
2022-03-03 17:21:46 +0200

    inceputul ex cinci, revin mai tarziu, DOM

---
+++ homework2/exfive.js
@@ -0,0 +1,12 @@
+//declaratia elementelor
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const inputt = document.getElementById("inputik");
+
+//prelucrarea
+
+//functia pentru plus
+
+function plusFunc() {
+   console.log( inputt.nodeValue);
+}
