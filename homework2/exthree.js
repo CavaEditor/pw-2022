a2e19e2727f5827cb4980cb8e99a55555d8a88bb
CavaEditor <cabacv15@gmail.com>
2022-03-03 16:28:29 +0200

    exercitiul trei DOM

---
+++ homework2/exthree.js
@@ -0,0 +1,10 @@
+//buttonul ORANGE schimba background-ul in orange
+
+//declararea buttonului
+const orange = document.getElementById("orange");
+
+//functia care schimba
+
+orange.addEventListener("click", () => {
+    document.body.style.background = "orange"
+})
