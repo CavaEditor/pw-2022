231a6ff58ef0f0d464389b3da09aea03cef22a96
CavaEditor <cabacv15@gmail.com>
2022-03-03 19:39:38 +0200

    incarcarea json-ului, format pe web-site-ul json ditor online:

--- homework3/exone.js
+++ homework3/exone.js
@@ -1,4 +1,5 @@
 //declararea json
+//de pe cloud https://jsoneditoronline.org/#left=cloud.0c404b9b22cf4243b5d473156f2bf0e5&right=local.yomoge
 const worker = {
     npp: {
         sureName: "nume",
@@ -8,13 +9,13 @@ const worker = {
     birthday: {
         bDate: 15,
         bMonth: "iunie",
-        bYear: 2001,
+        bYear: 2001
     },
     mobile: "+37367502300",
     adress: {
         city: "Balti",
         street: "Ostrovski",
-        number: 18,
+        number: 18
     },
     email: "cabacv15@gmail.com",
     gender: "f",
@@ -22,7 +23,7 @@ const worker = {
     dateStartWork: {
         sDate: 10,
         sMonth: "noiembrie",
-        sYear: 2021,
+        sYear: 2021
     },
     IDNP: "023165468",
     salary: 80000,
@@ -32,7 +33,7 @@ const worker = {
     homePhoneNumber: "022274161",
     workSchedule : {
         start: "15:00",
-        end: "19:00",
-    },
+        end: "19:00"
+    }
 }
 //const w = JSON.parse(worker);
